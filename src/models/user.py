6e7fed729a68f30e662853c06f468ca7a14aac59
miraz <miraz@evidentbd.com>
2024-01-19 03:43:19 +0600

    albamic setup complete for migration

--- src/models/user.py
+++ src/models/user.py
@@ -18,3 +18,17 @@ class User(Base):
     code = Column(String , nullable = True)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    
+    
+
+class person(Base):
+    __tablename__ = "person"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable= True)
+    age = Column(String , nullable = True)
+    country = Column(String , nullable = True)
+    city = Column(String ,nullable = True)
+    code = Column(String , nullable = True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
