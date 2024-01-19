fbbb50d1162fac7a3e9f672248d892e606e18c1a
miraz <mohammad.miraz@northsouth.edu>
2024-01-20 02:58:20 +0600

    update

--- src/apis/dropbox/views.py
+++ src/apis/dropbox/views.py
@@ -12,19 +12,43 @@ def start_auth_with_dropbox():
     
     print("I came here")
     return RedirectResponse(
-        url="https://github.com/login/oauth/authorize?client_id=c9878e72ce637c337a8c", status_code=302
+        url="https://www.dropbox.com/oauth2/authorize?client_id=sf1n10mvdi9lu2n&response_type=code&redirect_uri=http://localhost:8010/api/v1/dropbox/code", status_code=302
     )
 
 
-
+import requests
 def dropbox_code(query_params): 
+    
+    print(query_params)
     pass
     
    
+
+    url = "https://api.dropbox.com/oauth2/token"
+    authorization_code =  query_params.get('code')
+    redirect_uri = "http://localhost:8010/api/v1/dropbox/code"
+    client_id = "sf1n10mvdi9lu2n"
+    client_secret = "uaeqq1rrln7q6kx"
+
+    payload = {
+        'code': authorization_code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': redirect_uri,
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+
+    response = requests.post(url, data=payload)
+
+    print(response.text)
     
 
 
 def drop_box_success(query_params):
+    
+    print(query_params)
+    
+    
     pass
     
  
